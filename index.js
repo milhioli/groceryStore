408ddd4cc2f3775b164ed85b8680cbb59938ce35
mobiu <en.milhioli@gmail.com>
2025-01-20 13:27:19 -0300

    I will reset

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+const swiper = new Swiper('.card-wrapper', {
+    loop: true,
+    grabCursor: true,
+
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView:1
+        },
+        620: {
+            slidesPerView:2
+        },
+        1024: {
+            slidesPerView:3
+        }
+    }
+  });
+
+
 const chickenDialog = document.querySelector("#chickenModal");
 const chickenOp = document.querySelector("#chickenOpen");
 const chickenCl = document.querySelector("#chickenClose");
